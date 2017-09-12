e5849fe353beeae91d45b63d3941f9b8860f3dc8
Damian Reiter <damian.reiter@gmail.com>
2017-09-12 23:13:08 +0000

    All information on projects page is dynamic and depends on which project the user clicked on

--- app.js
+++ app.js
@@ -9,9 +9,34 @@ app.get("/", function(req, res){
 });
 
 app.get("/project/:projectName", function(req, res){
-    res.render("project", {projectName: req.params.projectName, albumSize: 0});
+    var desc;
+    var albumSize = 0;
+    var skills = [];
+    
+    switch(req.params.projectName){
+        case "easyweather":
+            desc = "This is the description for EasyWeather";
+            albumSize = 1;
+            skills = ["Java", "Volley", "OpenWeatherMap API", "JSON"];
+            break;
+        case "placefinder":
+            desc = "This is the description for PlaceFinder";
+            albumSize = 5;
+            skills = ["Java", "Volley", "JSON", "Google API Client", "Google Maps API", "Google Places API"];
+            break;
+    }
+    var project = {
+        title: req.params.projectName,
+        desc: desc,
+        albumSize: albumSize,
+        skills: skills
+    };
+    res.render("project", {project: project});
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server started on port " + process.env.PORT); 
 });
+
+// PROJECT INFORMATION
+
